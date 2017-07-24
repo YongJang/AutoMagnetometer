ea4bcc7937a04795c1c2d6efcc6bbec469b99539
YongJang <jyj3312@gmail.com>
2017-07-24 20:25:31 +0900

    add getScale()
    add getScale() so I can get exact location of touch point

--- app/src/main/java/io/github/yongjang/automagnetometer/CustomPhotoAttacher.java
+++ app/src/main/java/io/github/yongjang/automagnetometer/CustomPhotoAttacher.java
@@ -1,5 +1,6 @@
 package io.github.yongjang.automagnetometer;
 
+import android.graphics.RectF;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.ImageView;
@@ -24,19 +25,25 @@ public class CustomPhotoAttacher  extends PhotoViewAttacher implements View.OnTo
     }
 
     public boolean onLongClick(View v) {
+        System.out.println("CALL --> imageView.setOnTouchListener::onLongClick()");
         return false;
     }
 
     public boolean onTouch(View view, MotionEvent event) {
+        RectF rf;
         if (event.getAction() == MotionEvent.ACTION_DOWN) {
-            float x = (int) event.getX();
-            float y = (int) event.getY();
+            rf = getDisplayRect();
+            float x = ((int) event.getX() - rf.left) / getScale();
+            float y = ((int) event.getY() - rf.top) / getScale();
             TextView valueX = textViewX;
             TextView valueY = textViewY;
             valueX.setText(Float.toString(x));
             valueY.setText(Float.toString(y));
-            System.out.println("12313123123123212");
+            System.out.println("////" + rf.left + "///" + rf.top + "////");
         }
+
+        System.out.println("*****************" + getScale() + "*****************");
+
         System.out.println("CALL --> imageView.setOnTouchListener::onTouch()");
         return super.onTouch(view, event);
     }
