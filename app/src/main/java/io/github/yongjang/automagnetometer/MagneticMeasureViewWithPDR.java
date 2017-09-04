516bac52c0f37721570bb3cceb7281ad6c63a764
YongJang <jyj3312@gmail.com>
2017-09-04 20:31:15 +0900

    add android design suppor at gradle file

--- app/src/main/java/io/github/yongjang/automagnetometer/MagneticMeasureViewWithPDR.java
+++ app/src/main/java/io/github/yongjang/automagnetometer/MagneticMeasureViewWithPDR.java
@@ -8,6 +8,65 @@ import android.support.v7.app.AppCompatActivity;
  * Created by YongJang on 2017-09-02.
  */
 
+/**********************************************/
+/***************TEST CODE IMPORT***************/
+import android.Manifest;
+import android.app.Activity;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Environment;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.Window;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+
+
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+
+import static android.os.SystemClock.uptimeMillis;
+import static java.lang.Math.asin;
+import static java.lang.Math.atan2;
+import static java.lang.Math.cos;
+import static java.lang.Math.hypot;
+import static java.lang.Math.sin;
+/**********************************************/
+/**********************************************/
+
 public class MagneticMeasureViewWithPDR extends AppCompatActivity{
     public MagneticMeasureViewWithPDR() { }
 
